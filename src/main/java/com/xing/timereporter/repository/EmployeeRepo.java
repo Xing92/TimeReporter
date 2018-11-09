172c11e258812a6cd0d8298ba79995599c9def15
Kamil Bednarczyk <kamil.bednarczyk@connectis.pl>
2018-11-09 10:56:08 +0100

    s0ome changes to make stuff work

--- src/main/java/com/xing/timereporter/repository/EmployeeRepo.java
+++ src/main/java/com/xing/timereporter/repository/EmployeeRepo.java
@@ -1,8 +1,10 @@
 package com.xing.timereporter.repository;
 
+import com.xing.timereporter.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepo{// extends JpaRepository<Employee, Long> {
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
 }
